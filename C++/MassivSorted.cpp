52cd76ad3db8998f3b2551946117f2b53a576861
Edgar Grigoryan <edgar.grig.2001@gmail.com>
2018-04-09 23:05:38 +0400

    Homework 09.04.2018 Version.2

--- C++/MassivSorted.cpp
+++ C++/MassivSorted.cpp
@@ -11,8 +11,20 @@ int main () {
     int * arr = (int *) calloc(size, sizeof(int));
     srand(time(0)); 
     for (int i = 0; i < size; i++ ) {
-        cin >> arr[i];
+        arr[i] = rand() % 100 + 1;
     }
+    int min = arr[0];
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > arr[i+1]) {
+            for (int j = i; arr[j] > arr[j+1]; j--) {
+                arr[j] = arr[j] + arr[j+1];
+                arr[j+1] = arr[j] - arr[j+1];
+                arr[j] = arr[j] - arr[j+1];
+            }
+        }
+    }
+
     int number;
     cout << "Mtcreq tiv@: ";
     cin >> number;
