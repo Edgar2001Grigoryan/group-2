c57e563fda6d9823a86d842af4c8d9d620f80e1f
Edgar Grigoryan <edgar.grig.2001@gmail.com>
2018-04-09 22:03:06 +0400

    Homework 09.04.2018

---
+++ C++/MassivRand.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int main () {
+
+    int size;
+    cout << "Massisvi chaps@:";
+    cin >> size;
+    int * arr = (int *) calloc(size, sizeof(int));
+    srand(time(0)); 
+    for (int i = 0; i < size; i++ ) {
+        arr[i] = rand() % 100 + 1;
+    }
+    int number;
+    cout << "Mtcreq tiv@: ";
+    cin >> number;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == number) {
+            cout << (i+1) << " ";
+    }
+    }
+    cout << "\n";
+    for (int i = 0; i < size; i++) {
+        cout << "arr[" << (i+1) << "] = " << arr[i] << "\n";
+    }
+    return 0;
+
+}
