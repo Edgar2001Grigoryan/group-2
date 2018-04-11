d3963cc1eaa871088df5d4a167b80a1f0696bdc7
Edgar Grigoryan <edgar.grig.2001@gmail.com>
2018-04-11 21:50:48 +0400

    Homework 11.04.2018

--- C++/Struct.cpp
+++ C++/Struct.cpp
@@ -7,9 +7,10 @@ struct Person {
     string firstName;
     string lastName;
     int age;
-    string gender;
+    float weight;
+    bool gender;
     bool status;
-
+    bool maried;
 };
 
 int main () {
@@ -17,9 +18,13 @@ int main () {
     string firstNames[] = {"Michael","Tigran","Nvard","Mariam","Edgar","Sasha","Nona","Lilit","Hrach","Armen","Arpine","Ani","Artur"};
     string lastNames[] = {"Sargsyan","Hovhannisyan","Harutyunyan","Karapetyan","Grigoryan","Avagyan","Musaelian",
                           "Melkumyan","Gevorgyan","Abrahamyan","Harutyunyan","Hakobyan","Gasparyan"};
-    int ages[] = {17,21,21,21,16,21,21,21,21,21,21,21,25};
-    string genders[] = {"Male","Male","Female","Female","Male","Male","Female","Female","Male","Male","Female","Female","Male"};
+    int ages[] = {17,19,19,21,16,19,19,19,19,19,23,19,25};
+    string genderType[] = {"Female","Male"};
     bool statuses[] = {true,true,true,true,true,true,true,true,true,true,true,true,true};
+    bool genders[] = {true,true,false,false,true,true,false,false,true,true,false,false,true};
+    float weights[] = {53.5,82.5,51,50,65,70,55.5,48,72.5,70,47,51,61};
+    bool marieds[] = {false,false,false,false,false,false,false,false,false,false,false,false,false};
+    string mariedType[] = {"Not maried","Maried"};
 
     Person arr[13];
     for (int i = 0; i < 13; i++) {
@@ -29,6 +34,8 @@ int main () {
         m.age = ages[i];
         m.gender = genders[i];
         m.status = statuses[i];
+        m.weight = weights[i];
+        m.maried = marieds[i];
         arr[i] = m;
     }
     
@@ -44,16 +51,25 @@ int main () {
     for (int i = 0; i < 13; i++) {
         if (arr[i].firstName == firstName && arr[i].lastName == lastName) {
             k = true;
+            if (arr[i].maried == true && arr[i].gender == false) {
+                cout << "Missis ";
+            } else if (arr[i].maried == false && arr[i].gender == false) {
+                cout << "Miss ";
+            } else {
+                cout << "Mister ";
+            }
             cout << arr[i].firstName << " ";
             cout << arr[i].lastName << "\n";
             cout << "Age:" << arr[i].age << "\n";
-            cout << "Gender:" << arr[i].gender << "\n";
+            cout << "Weight:" << arr[i].weight << " kg\n";
+            cout << "Gender:" << genderType[arr[i].gender] << "\n";
             cout << "Status:";
             if (arr[i].status == true) {
-                cout << "Student";
+                cout << "Student" << "\n";
             } else {
-                cout << "Not student";
+                cout << "Not student" << "\n";
             }
+            cout << mariedType[arr[i].maried];
             break;
         }
     }
@@ -64,13 +80,25 @@ int main () {
     
     cout << "\n";
     for (int i = 0; i < 13; i++) {
-        cout << "\n" << arr[i].firstName << " " << arr[i].lastName << " " << arr[i].age << " " << arr[i].gender << " ";
-          if (arr[i].status == true) {
-                cout << "Student";
-            } else {
-                cout << "Not student";
-            }
-
+        cout << "\n";
+        if (arr[i].maried == true && arr[i].gender == false) {
+            cout << "Missis ";
+        } else if (arr[i].maried == false && arr[i].gender == false) {
+            cout << "Miss ";
+        } else {
+            cout << "Mister ";
+        }
+        cout << arr[i].firstName << " ";
+        cout << arr[i].lastName << "   ";
+        cout << arr[i].age << "   ";
+        cout << arr[i].weight << " kg   ";
+        cout << genderType[arr[i].gender] << "   ";
+        if (arr[i].status == true) {
+            cout << "Student   ";
+        } else {
+            cout << "Not student   ";
+        }
+        cout << mariedType[arr[i].maried];
     }
     return 0;
 }
