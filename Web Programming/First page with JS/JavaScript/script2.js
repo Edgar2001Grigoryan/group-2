c37c9bec868e5164c9412136935adb618d569417
Edgar Grigoryan <edgar.grig.2001@gmail.com>
2018-06-25 18:06:40 +0400

    Classwork 25.06.2018

--- Web Programming/First page with JS/JavaScript/script2.js
+++ Web Programming/First page with JS/JavaScript/script2.js
@@ -1,56 +1,135 @@
 window.onload = init;
 
 function init() {
-    if (window.localStorage.getItem("gender") == "Male") {
-        document.getElementById('toph1').innerHTML = "Hi Mr. " 
-        + window.localStorage.getItem("name") + " " + window.localStorage.getItem("surname");
+    count = 1;
+    if (window.sessionStorage.getItem("name") == null) {
+        document.getElementById('modal').style.display = "flex";
+    } else {
+        getData();
     }
-    if (window.localStorage.getItem("gender") == "Female") {
-        document.getElementById('toph1').innerHTML = "Hi Ms. " 
-        + window.localStorage.getItem("name") + " " + window.localStorage.getItem("surname");
+   
+    function getData() {
+        if (window.sessionStorage.getItem("gender") == "Male") {
+            document.getElementById('toph1').innerHTML = "Hi Mr. " 
+            + window.sessionStorage.getItem("name") + " " + window.sessionStorage.getItem("surname");
+        }
+        if (window.sessionStorage.getItem("gender") == "Female") {
+            document.getElementById('toph1').innerHTML = "Hi Mrs. " 
+            + window.sessionStorage.getItem("name") + " " + window.sessionStorage.getItem("surname");
+        }    
+
+        countn = 1;
+        while (window.sessionStorage.getItem("address" + String(countn))) {
+            add(10);
+            countn++;
+        }
     }
 
     document.getElementById('add').onclick = add;
-    var count = 1;
+    document.getElementById('button1').onclick = saveData;
 
-    function add() {
+    function add(x) {
         var node = document.createElement("div");
         node.setAttribute('id', String(count));
         document.getElementById("secondDiv").appendChild(node);
-        addContent();
+        addContent(x);
         count++;
     }
 
-    function addContent() {
+    function addContent(x) {
 
+
+        if (x != 10) {
+            window.sessionStorage.setItem("address" + String(count), document.getElementById('address').value);
+        }
         var node = document.createElement("h2");
-        var textnode = document.createTextNode("Address: " + document.getElementById('address').value);
+        if (window.sessionStorage.getItem("address" + String(countn))) {
+            var textnode = document.createTextNode("Address: " + window.sessionStorage.getItem("address" + String(countn)));
+        } else {
+            var textnode = document.createTextNode("Address: " + document.getElementById('address').value);
+        }
         node.appendChild(textnode);
         document.getElementById(String(count)).appendChild(node);
 
+        if (x != 10) {
+            window.sessionStorage.setItem("city" + String(count), document.getElementById('city').value);
+        }
         var node = document.createElement("h2");
-        var textnode = document.createTextNode("City: " + document.getElementById('city').value);
+        if (window.sessionStorage.getItem("city" + String(countn))) {
+            var textnode = document.createTextNode("City: " + window.sessionStorage.getItem("city" + String(countn)));
+        } else {
+            var textnode = document.createTextNode("City: " + document.getElementById('city').value);
+        }        
         node.appendChild(textnode);
         document.getElementById(String(count)).appendChild(node);
 
+        if (x != 10) {
+            window.sessionStorage.setItem("country" + String(count), document.getElementById('country').value);
+        }
         var node = document.createElement("h2");
-        var textnode = document.createTextNode("Country: " + document.getElementById('country').value);
+        if (window.sessionStorage.getItem("country" + String(countn))) {
+            var textnode = document.createTextNode("Country: " + window.sessionStorage.getItem("country" + String(countn)));
+        } else {
+            var textnode = document.createTextNode("Country: " + document.getElementById('country').value);
+        }
         node.appendChild(textnode);
         document.getElementById(String(count)).appendChild(node);
 
+        if (x != 10) {
+            window.sessionStorage.setItem("phone" + String(count), document.getElementById('phone').value);
+        }
         var node = document.createElement("h2");
-        var textnode = document.createTextNode("Phone: " + document.getElementById('phone').value);
+        if (window.sessionStorage.getItem("phone" + String(countn))) {
+            var textnode = document.createTextNode("Phone: " + window.sessionStorage.getItem("phone" + String(countn)));
+        } else {
+            var textnode = document.createTextNode("Phone: " + document.getElementById('phone').value);
+        }        
         node.appendChild(textnode);
         document.getElementById(String(count)).appendChild(node);
 
+        if (x != 10) {
+            window.sessionStorage.setItem("name" + String(count), document.getElementById('name').value);
+        }
         var node = document.createElement("h2");
-        var textnode = document.createTextNode("Name: " + document.getElementById('name').value);
-        node.appendChild(textnode);
+        if (window.sessionStorage.getItem("name" + String(countn))) {
+            var textnode = document.createTextNode("Name: " + window.sessionStorage.getItem("name" + String(countn)));
+        } else {
+            var textnode = document.createTextNode("Name: " + document.getElementById('name').value);
+        }        node.appendChild(textnode);
         document.getElementById(String(count)).appendChild(node);
 
+        if (x != 10) {
+            window.sessionStorage.setItem("surname" + String(count), document.getElementById('surname').value);
+        }
         var node = document.createElement("h2");
-        var textnode = document.createTextNode("Surname: " + document.getElementById('surname').value);
-        node.appendChild(textnode);
+        if (window.sessionStorage.getItem("surname" + String(countn))) {
+            var textnode = document.createTextNode("Surname: " + window.sessionStorage.getItem("surname" + String(countn)));
+        } else {
+            var textnode = document.createTextNode("Surname: " + document.getElementById('surname').value);
+        }        node.appendChild(textnode);
         document.getElementById(String(count)).appendChild(node);
+    }
+
+    function saveData() {
+        name = document.getElementById('name1').value;
+        surname = document.getElementById('surname1').value;
+        gender = null;
+        if (checkbox1.checked) {
+            gender = "Male";
+        }
+        if (checkbox2.checked) {
+            gender = "Female";
+        }
+        window.sessionStorage.setItem("name", name);
+        window.sessionStorage.setItem("surname", surname);
+        window.sessionStorage.setItem("gender", gender);    
+        document.getElementById('modal').style.display = "none";
+        getData();
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
